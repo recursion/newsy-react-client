43621bea5a8dde24ace753a2105cf1784929fa3c
Michael Symmes <msymmes@gmail.com>
2018-08-19 14:33:55 -0400

    stopped JSON.parsing stories as its sent properly.

--- app/containers/SearchPage/actions.js
+++ app/containers/SearchPage/actions.js
@@ -50,10 +50,9 @@ export function loadStories() {
  * @return {object}      An action object with a type of LOAD_STORIES passing the stories 
  */
 export function storiesLoaded(stories) {
-  console.log(typeof stories);
   return {
     type: LOAD_STORIES_SUCCESS,
-    stories: JSON.parse(stories)
+    stories: stories
   };
 }
 
