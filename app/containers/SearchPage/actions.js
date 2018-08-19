b897e5aeb01ddf820458ca3658eff093cf13b9b3
Michael Symmes <msymmes@gmail.com>
2018-08-18 23:31:13 -0400

    Updated api docs.

--- app/containers/SearchPage/actions.js
+++ app/containers/SearchPage/actions.js
@@ -62,7 +62,7 @@ export function storiesLoaded(stories) {
  *
  * @param  {object} error The error
  *
- * @return {object}       An action object with a type of LOAD_STORIES_ERROR passing the error
+ * @return {object} An action object with a type of LOAD_STORIES_ERROR passing the error
  */
 export function storiesLoadingError(error) {
   return {
