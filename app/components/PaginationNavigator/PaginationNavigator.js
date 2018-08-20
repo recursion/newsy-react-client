a57d9e305ebe5b2d9597e23261f7d863b6202a78
Michael Symmes <msymmes@gmail.com>
2018-08-20 14:08:54 -0400

    Styling and clode Cleanup.

--- app/components/PaginationNavigator/PaginationNavigator.js
+++ app/components/PaginationNavigator/PaginationNavigator.js
@@ -4,48 +4,49 @@ import PropTypes from 'prop-types';
 import './style';
 
 const PaginationNavigator = ({page, totalStories, onGetPage}) => {
-  // calculate how many pages are available
-  // and build a widget for navigating them.
   const control = (str, pageNumber, selected) => {
-    const buildClass = (selected) ? "pagination-nav__control selected" : "pagination-nav__control";
-    return (
-      // we likely want to change this to a link?
-      // either way it should probably be its own component.
-      // <button onClick={() => onGetPage(p)}>{str}</button>
-      <button key={pageNumber} onClick={() => onGetPage(pageNumber)} className={buildClass}>
-        {str}
-      </button>
-    )
+    if (selected) {
+      return <span className="pagination-nav__control selected">{str}</span>
+    } else {
+      return (
+        <button 
+          key={pageNumber} 
+          onClick={() => onGetPage(pageNumber)} 
+          className="pagination-nav__control"
+        >
+          {str}
+        </button>
+      )
+    }
   }
   const createLinks = (currentPage, totalPages) => {
     const result = [];
+    const addControl = (i) => {
+      result.push(control(i+'', i, (currentPage === i)));
+    }
+    if (totalPages <= 1) {
+      addControl(1);
+    }
     if (totalPages < 10) {
       for (let i = 1; i < totalPages; i++) {
-        const selected = (currentPage === i) ? true : false;
-        result.push(control(i+'', i, selected));
+        addControl(i);
       }
     } else if (totalPages >= 10 && currentPage < 6) {
       for (let i = 1; i < 11; i++) {
-        const selected = (currentPage === i) ? true : false;
-        result.push(control(i+'', i, selected));
+        addControl(i);
       }
     } else {
       for (let i = currentPage - 5; i < currentPage + 5; i++) {
-        const selected = (currentPage === i) ? true : false;
-        result.push(control(i+'', i, selected));
+        addControl(i);
       }
     }
     return result;
   }
-  const totalPages = Math.round(totalStories / 20);
+
+  const totalPages = Math.round(totalStories / 20) + 1;
   return (
     <div className="pagination-nav centerText">
       {(page > 1) ? control('Previous', page - 1) : ''} 
-      {/* 
-            we need determine how many pages are available
-            we will show up to 10 links for pages
-            labeled by their page number
-      */}
       {createLinks(page, totalPages)}
       {(page < totalPages) ? control('Next', page + 1) : ''} 
     </div>
