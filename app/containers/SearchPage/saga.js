a919c4207049996174e15abf4588ae575531ce03
Michael Symmes <msymmes@gmail.com>
2018-08-19 23:49:28 -0400

    Changed to watch multiple actions.

--- app/containers/SearchPage/saga.js
+++ app/containers/SearchPage/saga.js
@@ -30,10 +30,10 @@ export function* getStories() {
 /**
  * Root saga manages watcher lifecycle
  */
-export default function* stories () {
+export default function* watchAll () {
   // Watches for LOAD_STORIES actions and calls getRepos when one comes in.
   // By using `takeLatest` only the result of the latest API call is applied.
   // It returns task descriptor (just like fork) so we can continue execution
   // It will be cancelled automatically on component unmount
-  yield takeLatest(LOAD_STORIES, getStories);
+  yield [takeLatest(LOAD_STORIES, getStories)];
 }
