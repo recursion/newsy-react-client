efcf6173b6b1e6835a73305666c6868e55bb5ad0
Michael Symmes <msymmes@gmail.com>
2018-08-19 21:05:49 -0400

    Removed 'made by' section in footer.

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -4,7 +4,6 @@ import './style.scss';
 const Footer = () => (
   <footer>
     <section>Powered by <a href="https://newsapi.org">newsapi.org</a></section>
-    <section>Made with <span role="img" aria-label="heart-emoji">❤️</span> by <a href="https://github.com/recursion">Michael Symmes</a></section>
     <section>Licensed under the MIT license.</section>
   </footer>
 );
