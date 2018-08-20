ca3014d65d0865ac1a7686ea2048499e08aadb37
Michael Symmes <msymmes@gmail.com>
2018-08-19 20:19:48 -0400

    Update footer messages and layout.

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -3,8 +3,9 @@ import './style.scss';
 
 const Footer = () => (
   <footer>
-    <section>This project is licensed under the MIT license.</section>
+    <section>Powered by <a href="https://newsapi.org">newsapi.org</a></section>
     <section>Made with <span role="img" aria-label="heart-emoji">❤️</span> by <a href="https://github.com/recursion">Michael Symmes</a></section>
+    <section>Licensed under the MIT license.</section>
   </footer>
 );
 
