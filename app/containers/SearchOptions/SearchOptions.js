bb7898a4b16214848113f74f5d8716f1e50bff94
Michael Symmes <msymmes@gmail.com>
2018-08-23 16:09:13 -0400

    Moved advancedSearchOptions back into SearchOptions.

--- app/containers/SearchOptions/SearchOptions.js
+++ app/containers/SearchOptions/SearchOptions.js
@@ -10,22 +10,39 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {} from './constants';
 import './style.scss';
-import AdvancedSearchOptions from './AdvancedSearchOptions';
+import TargetOptions from './TargetOptions';
+import SourceOptions from './SourceOptions';
+import SpecialOptions from './SpecialOptions';
 
 export default class SearchOptions extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
-    const advancedOptionProps = {
-      toggleSearchType: this.props.toggleSearchType,
-      useSources: this.props.useSources,
-      toggleUseSources: this.props.toggleUseSources,
-      country: this.props.country,
-      target: this.props.target,
-      onChangeCountry: this.props.onChangeCountry,
-      onChangeTarget: this.props.onChangeTarget
-    };
-
     if (this.props.advanced) {
-      return <AdvancedSearchOptions {...advancedOptionProps} />;
+      return (
+        <section className="search-options">
+          <TargetOptions
+            target={this.props.target}
+            onChangeTarget={this.props.onChangeTarget}
+          />
+          {(this.props.useSources) ?
+            <SourceOptions
+              target={this.props.target}
+              toggleUseSources={this.props.toggleUseSources}
+            /> :
+            <SpecialOptions
+              target={this.props.target}
+              country={this.props.country}
+              onChangeCountry={this.props.onChangeCountry}
+              toggleUseSources={this.props.toggleUseSources}
+            />
+          }
+          <button
+            className="search-options__type"
+            onClick={() => this.props.toggleSearchType()}
+          >
+              Return to Simple Search
+          </button>
+        </section>
+      );
     }
     return (
       <section className="search-options">
