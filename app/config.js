db87c948a8f8b046c4227b789bee67d6f2991e02
Michael Symmes <msymmes@gmail.com>
2018-08-28 12:34:39 -0400

    Added environment checks for URL.

--- app/config.js
+++ app/config.js
@@ -1,6 +1,11 @@
 
+const devURL = 'http://localhost:3000/v1';
+const prodURL = 'https://myserver.com/v1';
+
 const config = {
-  url: 'http://localhost:3000/v1'
+  // this should be changed to wherever the newsy api server is running
+  // for development I usually use 'http://localhost:3000/v1'
+  url: (process.env.NODE_ENV === 'development') ? devURL : prodURL
 };
 
 export default config;
