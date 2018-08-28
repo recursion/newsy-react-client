b5863d549fea07d473137674d3fbd75524439e9c
Michael Symmes <msymmes@gmail.com>
2018-08-27 20:45:18 -0400

    Cleaned up usageTips styles and wording.

--- app/containers/UsageTips/UsageTips.js
+++ app/containers/UsageTips/UsageTips.js
@@ -29,13 +29,13 @@ export default function UsageTips() {
         <h4 className="subtitle">Advanced Option Settings</h4>
       </div>
       <ul className="box">
-        <li><span>Target:</span>Top stories/headlines or everything. Default is everything.</li>
+        <li><span>Target:</span>Top stories/headlines or everything. Default is everything. Depending on your selection here, some of the following options may or may not be available.</li>
         <li><span>Language:</span>Select the language you want stories to be chosen from. Default is all.</li>
+        <li><span>From Date:</span>Select the date you want to start searching from. Default is oldest available.</li>
+        <li><span>To Date:</span>Select the date you want to stop searching at. Default is newest available.</li>
         <li><span>Country:</span>Select the country you want news from. Default is all. Only available in Top Stories/headline search. (cannot be used with sources)</li>
         <li><span>Category:</span>Select the category you want news from. Default is all. Only available in Top Stories/headlines search. (cannot be used with sources)</li>
         <li><span>Sources:</span>Select the sources you want news from. Default is all. (cannot be used with country and/or category)</li>
-        <li><span>From Date:</span>Select the date you want to start searching from. Default is oldest available.</li>
-        <li><span>To Date:</span>Select the date you want to stop searching at. Default is newest available.</li>
       </ul>
     </div>
   );
